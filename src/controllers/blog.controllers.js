83b3d9f5b3a499de60993632938a7541f492dd1f
kushwahramkumar2003 <kushwahramkumar2003gmail.com>
2024-01-02 17:56:14 +0530

    Increase number of blogs displayed

--- src/controllers/blog.controllers.js
+++ src/controllers/blog.controllers.js
@@ -7,7 +7,7 @@ exports.getBlogs = async (req, res) => {
       `${config.BLOGS_API_DOMAIN}/api/v1/posts?searchKeyword`
     );
 
-    blogs.data = blogs.data.slice(0, 3);
+    blogs.data = blogs.data.slice(0, 6);
 
     res.status(200).json(blogs.data);
   } catch (error) {
